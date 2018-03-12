ec6769e21c83ff63f5b78bb41889a1a63b9e66f2
Jamesc974 <36702319+Jamesc974@users.noreply.github.com>
2018-03-12 14:06:00 +0100

    Add files via upload

---
+++ .gitignore/index.js
@@ -0,0 +1,94 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
+const db = require('quick.db');
+version = "0.1 🎉";
+draincorp = "TarKyo 👔";
+prefix = botconfig.prefix;
+
+// 💻Développeur, ✏ Graphiste, 🔧Modélisateur, 🏢Builder, 🎮 Membre
+
+bot.on('guildMemberAdd', member => {
+  console.log('User ' + member.username + 'à rejoins le serveur ')
+  var role = member.guild.roles.find('name', '🎮 Membre');
+  member.addRole(role)
+})
+
+bot.on('message', message => {
+  if (message.content === '!graphiste') {
+    var RoleToAdd = message.guild.roles.find('name', '✏ Graphiste');
+    var role = new Discord.RichEmbed()
+    .setDescription(`:white_check_mark: **${message.author.tag}** vient de rejoindre le groupe de **Graphiste** `)
+    .setColor("#009900");
+    message.channel.fetchMessages({limit: 1}).then(messages => message.channel.bulkDelete(messages));
+    message.member.addRole(RoleToAdd);
+    message.channel.sendMessage(role).then(msg => msg.delete(5000));
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === '!modélisateur') {
+    var RoleToAdd = message.guild.roles.find('name', '🔧Modélisateur');
+    var role = new Discord.RichEmbed()
+    .setDescription(`:white_check_mark: **${message.author.tag}** vient de rejoindre le groupe de **Modélisateur** `)
+    .setColor("#009900");
+    message.channel.fetchMessages({limit: 1}).then(messages => message.channel.bulkDelete(messages));
+    message.member.addRole(RoleToAdd);
+    message.channel.sendMessage(role).then(msg => msg.delete(5000));
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === '!développeur') {
+    var RoleToAdd = message.guild.roles.find('name', '💻Développeur');
+    var role = new Discord.RichEmbed()
+    .setDescription(`:white_check_mark: **${message.author.tag}** vient de rejoindre le groupe de **Développeur** `)
+    .setColor("#009900");
+    message.channel.fetchMessages({limit: 1}).then(messages => message.channel.bulkDelete(messages));
+    message.member.addRole(RoleToAdd);
+    message.channel.sendMessage(role).then(msg => msg.delete(5000));
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === '!builder') {
+    var RoleToAdd = message.guild.roles.find('name', '🏢Builder');
+    var role = new Discord.RichEmbed()
+    .setDescription(`:white_check_mark: **${message.author.tag}** vient de rejoindre le groupe de **Builder** `)
+    .setColor("#009900");
+    message.channel.fetchMessages({limit: 1}).then(messages => message.channel.bulkDelete(messages));
+    message.member.addRole(RoleToAdd);
+    message.channel.sendMessage(role).then(msg => msg.delete(5000));
+  }
+});
+
+// 💻Développeur, ✏ Graphiste, 🔧Modélisateur, 🏢Builder, 🎮 Membre
+bot.on('message', message => {
+  if (message.content === '!leave') {
+    var RoleToAdd = message.guild.roles.find('name', '🎮 Membre');
+    let gRole = message.guild.roles.find(`name`,'✏ Graphiste');
+    let mRole = message.guild.roles.find(`name`,'🔧Modélisateur');
+    let bRole = message.guild.roles.find(`name`,'🏢Builder');
+    let dRole = message.guild.roles.find(`name`,'💻Développeur');
+    var role = new Discord.RichEmbed()
+    .setDescription(`:x: **${message.author.tag}** est maintenant **Membre** `)
+    .setColor("#009900");
+    message.channel.fetchMessages({limit: 1}).then(messages => message.channel.bulkDelete(messages));
+    message.member.addRole(RoleToAdd);
+    message.member.removeRole(gRole);
+    message.member.removeRole(mRole);
+    message.member.removeRole(bRole);
+    message.member.removeRole(dRole);
+    message.channel.sendMessage(role).then(msg => msg.delete(5000));
+  }
+});
+
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} est en ligne sur ${bot.guilds.size} serveurs!`);
+
+});
+
+bot.login('NDIyNzMyNDc3NDEzNDU3OTIw.DYgD4w.eoPXCi8KWQk2s4yA7trZk7SF3Mw');
