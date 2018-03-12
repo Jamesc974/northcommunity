2ba1ba5d37967fc45d2b1f24e82d1ba1ecf12477
Jamesc974 <36702319+Jamesc974@users.noreply.github.com>
2018-03-12 17:25:20 +0100

    Add files via upload

---
+++ .gitignore/commands/annonce.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let aUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!aUser) return;
+    let areason = args.join(" ").slice(22)
+    let iconm = bot.user.avatarURL;
+
+    let annonceEmbed = new Discord.RichEmbed()
+    .setAuthor("📋 Annonce", iconm)
+    .setColor("#A901DB")
+    .addField("Nouvelle annonce", areason);
+
+    let annonceschannel = message.guild.channels.find(`name`, "bot-message");
+    if(!annonceschannel) return message.channel.send("❗ Impossible de trouver le canal des rapports.");
+
+
+    message.delete().catch(O_o=>{});
+    annonceschannel.send(annonceEmbed);
+
+}
+ 
+module.exports.help = {
+  name: "annonce"
+}
