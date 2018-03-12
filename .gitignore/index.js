8f5f723eb39160715ff9183aae1003c41529755c
Jamesc974 <36702319+Jamesc974@users.noreply.github.com>
2018-03-12 14:09:45 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,6 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
-const db = require('quick.db');
 version = "0.1 🎉";
 draincorp = "TarKyo 👔";
 prefix = botconfig.prefix;
